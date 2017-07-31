43ae561376d864a8b4a5ff425727559b17c34acf
Marina <marinadezordi@gmail.com>
2017-07-31 18:34:15 -0300

    url

--- R/temperatura.R
+++ R/temperatura.R
@@ -1,7 +1,10 @@
 #setwd("")
 getTemperatura <- function() {
   #carregando as informações
-  load("~/RCPTEC/R/data/temperatura.Rda") # temperatura
+  #load("~/RCPTEC/R/data/temperatura.Rda") # temperatura
+
+  arq = url("https://github.com/marinadezordi/RCPTEC/raw/master/R/data/temperatura.Rda")
+  load(arq)
   #dados
   temp <- temperatura[50:70, 17]
   print(temp)
