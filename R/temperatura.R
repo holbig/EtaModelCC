dcc555310ab49024f668532c56083abbb2e4d547
Marina <marinadezordi@gmail.com>
2017-10-25 21:52:44 -0200

    Alterações

--- R/temperatura.R
+++ R/temperatura.R
@@ -39,9 +39,9 @@ getWeatherHour<- function(lon1, lat1, iTime1, fTime1){
   humi = getWeatherData(lon = lon1, lat = lat1, iTime = iTime1, fTime = fTime1, weatherData = 'UR2M')
   wind = getWeatherData(lon = lon1, lat = lat1, iTime = iTime1, fTime = fTime1, weatherData = 'V10M')
 
- # df <- data.frame(Temperature = temp, Radiation = oc, Precipitation = precip, Humidity = humi, Wind = wind)
+  # df <- data.frame(Temperature = temp, Radiation = oc, Precipitation = precip, Humidity = humi, Wind = wind)
 
-  w = rbind(Temperature = temp, Radiation = oc, Precipitation = precip, Humidity = humi, Wind = wind)
+  w = rbind(Temperature = temp, Radiation = c, Precipitation = precip, Humidity = humi, Wind = wind)
   return(w)
 }
 w = getWeatherHour(lon = -50.88,lat = -28.51, iTime = '2017080700')
@@ -67,6 +67,28 @@ ajuste_ponto <- function(dados,longitude,latitude){
   return(c(ponto_lon,ponto_lat))
 }
 
+library(jsonlite)
+
+getClimateData<- function(modelID, modelFrequency, modelVar, lon, lat, iMonth = 1, iYear, fMonth = 1, fYear) {
+
+  modelOption <- switch(modelFrequency,
+                        HOURLY = '/1',
+                        DAILY = '/2',
+                        MONTHLY = '/3',
+                        YEARLY = '/4')
+
+  api <- paste("https://projeta.cptec.inpe.br/api/v1/public/ETA/", modelID, "/", modelFrequency, modelOption, "/", iMonth, "/", iYear, "/", fMonth, "/", fYear, "/", modelVar, "/", lon, '/',lat,'/', sep="")
+  model_data <- fromJSON(api)
+  model_output <- list(Model = modelID, Frequency = modelFrequency, Value = model_data$value,
+                       Year = model_data$date)
+  model_output
+
+}
+
+teste <- getClimateData('1', 'YEARLY','TP2M', '-12', '-49', 1, 2006, 12, 2010)
+teste$Year = c(2006:2010)
+getClimateData('1', 'YEARLY','TP2M', '-12', '-49', iYear=2006, fYear=2010)
+
 loadRda <- function(fileName){
   load(paste("~/RCPTEC/R/data/", fileName, ".Rdata", sep = ""))
   get(ls()[ls() != "fileName"])
@@ -74,23 +96,28 @@ loadRda <- function(fileName){
 
 info.RCPTEC <- function(){
 
-cat(paste("CPTEC/INPE",
-"Eta Model 15km Brazil",
+  cat(paste("CPTEC/INPE",
+            "Eta Model 15km Brazil",
 
-"AREA COVERED IN THE MODEL",
-"LONGITUDE:   -75.05 to -33.95,",
-"LATITUDE:   -35.05 to 5.90",
+            "AREA COVERED IN THE MODEL",
+            "LONGITUDE:   -75.05 to -33.95,",
+            "LATITUDE:   -35.05 to 5.90",
 
-"Variables:",
-"- V10M - wind up 10m (m/s)",
-"- TP2M - temperature up 2m",
-"- PREC - precipitation",
-"- UR2M - humidity",
-"- OCIS - solar radiation",
+            "Variables:",
+            "- V10M - wind up 10m (m/s)",
+            "- TP2M - temperature up 2m",
+            "- PREC - precipitation",
+            "- UR2M - humidity",
+            "- OCIS - solar radiation",
 
-"FORECAST PERIOD (11 days)",
-"YYYYMMDDHH to YYYYMMDDHH (Initial YearMonthDayHour to Final YearMonthDayHour)",  sep='\n'))
+            "FORECAST PERIOD (11 days)",
+            "YYYYMMDDHH to YYYYMMDDHH (Initial YearMonthDayHour to Final YearMonthDayHour)",  sep='\n'))
 }
 
 info.RCPTEC()
+
+library(jsonlite)
+api <- fromJSON("https://projeta.cptec.inpe.br/api/v1/public/ETA/1/YEARLY/4/1/2006/12/2010/TP2M/-12/-49/")
+api <- unname(api)
+a <- data.table(LONGITUDE=api[6])
 
