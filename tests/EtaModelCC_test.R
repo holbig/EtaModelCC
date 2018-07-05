47ab8efecaa72cab2a903b240ef6673914dfc8af
Carlos Amaral Holbig <carlos.holbig@gmail.com>
2018-07-05 15:22:24 -0300

    Atualizando testes.

--- tests/EtaModelCC_test.R
+++ tests/EtaModelCC_test.R
@@ -4,8 +4,8 @@ library(EtaModelCC)
 getInfoClimate()
 
 # Tests for getClimateData() function
-getClimateData('1', 'YEARLY', 'TP2M', '-28.263', '-52.407', iYear = 2006, fYear = 2010)
-getClimateData('1', 'YEARLY', 'TP2M', '-28.263', '-52.407', 1, 2006, 12, 2010)
-getClimateData('1', 'MONTHLY','TP2M', '-28.263', '-52.407', 1, 2006, 12, 2006)
-getClimateData('1', 'DAILY',  'TP2M', '-28.263', '-52.407', 1, 2006, 01, 2006)
-getClimateData('1', 'HOURLY', 'TP2M', '-28.263', '-52.407', 1, 2006, 01, 2006)
+getClimateData('1', 'YEARLY', 'TP2M', '-28.35', '-52.35', iYear = 2006, fYear = 2010)
+getClimateData('1', 'YEARLY', 'TP2M', '-28.35', '-52.35', 1, 2006, 12, 2010)
+getClimateData('1', 'MONTHLY','TP2M', '-28.35', '-52.35', 1, 2006, 12, 2006)
+getClimateData('1', 'DAILY',  'TP2M', '-28.35', '-52.35', 1, 2006, 01, 2006)
+getClimateData('1', 'HOURLY', 'TP2M', '-28.35', '-52.35', 1, 2006, 01, 2006)
