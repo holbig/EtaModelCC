7f7479414528e4c9063d4e71e42b879284672607
Carlos Amaral Holbig <carlos.holbig@gmail.com>
2019-01-11 15:15:12 -0200

    Atualizando arquivo de testes

--- tests/EtaModelCC_test.R
+++ tests/EtaModelCC_test.R
@@ -5,18 +5,13 @@ getInfoClimate()
 
 # Tests for getClimateData() function
 
-# getClimateData(modelID, modelFrequency, modelVar, lat, lon, iMonth = 1, iYear, fMonth = 1, fYear)
+# getClimateData(modelID, modelFrequency, modelVar, lat, lon, iYear, fYear)
 getClimateData('1', 'YEARLY', 'TP2M', '-28.35', '-52.34', iYear = 2006, fYear = 2010)
-getClimateData('1', 'YEARLY', 'TP2M', '-28.35', '-52.35', 2006, 2010)
-getClimateData('1', 'MONTHLY','TP2M', '-28.35', '-52.35', 2006, 2008)
+getClimateData('3', 'YEARLY', 'TP2M', '-28.35', '-52.35', 2006, 2010)
+getClimateData('5', 'MONTHLY','TP2M', '-28.35', '-52.35', 2006, 2008)
 getClimateData('1', 'DAILY',  'TP2M', '-28.35', '-52.35', 2006, 2006)
 getClimateData('1', 'HOURLY', 'TP2M', '-28.35', '-52.35', 2006, 2006)
 
-getClimateData('1', 'HOURLY', 'TP2M', '-28.2612', '-52.4083', 2018, 2018)
-
-
-getClimateData('4', 'YEARLY', 'TP2M', '-23.5', '-46.6', 2018, 2048)
-getClimateData('5', 'DAILY', 'TP2M', '-23.5', '-46.6', 2018, 2018)
-
+# getClimateDataBr(modelID, modelFrequency, modelVar, iYear, fYear)
 getClimateDataBR('1', 'YEARLY', 'TP2M', iYear = 2006, fYear = 2006)
 
